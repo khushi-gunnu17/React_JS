265ca91ad11bfddf3c26d3c82626a5a2a9463397
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-01-13 13:45:24 +0530

    React

--- 08_miniContext/src/context/UserContext.js
+++ 08_miniContext/src/context/UserContext.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // By wrapping any data with the context, it becomes a provider.
 // Global User Context = the data will get access to this global variable.
+// Redux = organinzing the state management
 
 const UserContext = React.createContext()
 
