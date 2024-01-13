821f842e19e0143587f5b13cf0df1b0510a0b959
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-01-13 17:51:50 +0530

    React

--- 08_miniContext/src/components/Profile.jsx
+++ 08_miniContext/src/components/Profile.jsx
@@ -4,6 +4,7 @@ import UserContext from '../context/UserContext'
 function Profile() {
     const {user} = useContext(UserContext)              // asking for the user values
 
+    // conditional return
     if (!user) return <div>Please Login !</div>
 
     return <div>Welcome {user.username} </div>
